52e6ebcf25ad9b665e50bfdc41b1b10888efffbf
watakooohhh3439 <bochuwatakoh3439@gmail.com>
2026-01-02 17:14:26 +0900

    プロジェクトファイルを追加。

---
+++ ArucoPointer/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ArucoPointer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(39, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(729, 363);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(319, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 391);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 59);
+            this.panel1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(530, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ArucoPointer/Form1.cs
@@ -0,0 +1,136 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp.Extensions;
+using OpenCvSharp.Aruco;
+
+namespace ArucoPointer
+{
+    public partial class Form1 : Form
+    {
+        // ★修正ポイント1：変数をここで宣言して、ボタンからもカメラ処理からも見えるようにする（クラスのメンバ変数にする）
+        List<Mat> collectedRvecs = new List<Mat>(); // 回転データ
+        List<Mat> collectedTvecs = new List<Mat>(); // 位置データ
+        bool isCalibrating = false; // キャリブレーション中かどうか
+        int collectCount = 0; // 集めた数
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("カメラを起動します！");
+            Task.Run(() => CameraLoop());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // ここで変数を触ってもエラーにならなくなります
+            collectedRvecs.Clear();
+            collectedTvecs.Clear();
+            collectCount = 0;
+            isCalibrating = true;
+            MessageBox.Show("先端を固定して、指示棒を回しながらデータを集めます。\nOKを押すと開始します。");
+        }
+
+        private void CameraLoop()
+        {
+            // パラメータ設定
+            double fx = 600;
+            double cx = 320;
+            double cy = 240;
+            float markerLength = 0.05f;
+
+            using (var cameraMatrix = new Mat(3, 3, MatType.CV_64FC1))
+            using (var distCoeffs = new Mat(5, 1, MatType.CV_64FC1, new Scalar(0)))
+            using (var capture = new VideoCapture(0))
+            {
+                cameraMatrix.Set<double>(0, 0, fx);
+                cameraMatrix.Set<double>(0, 1, 0);
+                cameraMatrix.Set<double>(0, 2, cx);
+                cameraMatrix.Set<double>(1, 0, 0);
+                cameraMatrix.Set<double>(1, 1, fx);
+                cameraMatrix.Set<double>(1, 2, cy);
+                cameraMatrix.Set<double>(2, 0, 0);
+                cameraMatrix.Set<double>(2, 1, 0);
+                cameraMatrix.Set<double>(2, 2, 1.0);
+
+                if (!capture.IsOpened())
+                {
+                    MessageBox.Show("カメラが見つかりません！");
+                    return;
+                }
+
+                using (var mat = new Mat())
+                {
+                    var dictionary = CvAruco.GetPredefinedDictionary(PredefinedDictionaryName.Dict4X4_50);
+                    var parameters = new DetectorParameters();
+
+                    while (true)
+                    {
+                        capture.Read(mat);
+                        if (mat.Empty()) continue;
+
+                        CvAruco.DetectMarkers(mat, dictionary, out var corners, out var ids, parameters, out var rejected);
+
+                        if (ids.Length > 0)
+                        {
+                            CvAruco.DrawDetectedMarkers(mat, corners, ids);
+
+                            using (var rvecs = new Mat())
+                            using (var tvecs = new Mat())
+                            {
+                                CvAruco.EstimatePoseSingleMarkers(corners, markerLength, cameraMatrix, distCoeffs, rvecs, tvecs);
+
+                                for (int i = 0; i < ids.Length; i++)
+                                {
+                                    Cv2.DrawFrameAxes(mat, cameraMatrix, distCoeffs, rvecs.Row(i), tvecs.Row(i), 0.1f);
+                                }
+
+                                // ★修正ポイント2：rvecsが生きているこのブロックの中に処理を入れる！
+                                if (isCalibrating)
+                                {
+                                    // データをコピーして保存
+                                    collectedRvecs.Add(rvecs.Row(0).Clone());
+                                    collectedTvecs.Add(tvecs.Row(0).Clone());
+                                    collectCount++;
+
+                                    Console.WriteLine($"データ収集中... {collectCount}/20");
+
+                                    // 20個たまったら終了
+                                    if (collectCount >= 20)
+                                    {
+                                        isCalibrating = false;
+                                        MessageBox.Show("データ収集完了！これから計算します（未実装）");
+                                    }
+
+                                    // データの偏りを防ぐため待機
+                                    Thread.Sleep(100);
+                                }
+                            } // ← rvecs, tvecs はここで寿命が終わります
+                        }
+
+                        pictureBox1.Invoke((Action)(() =>
+                        {
+                            var oldImage = pictureBox1.Image;
+                            pictureBox1.Image = BitmapConverter.ToBitmap(mat);
+                            if (oldImage != null) oldImage.Dispose();
+                        }));
+
+                        Thread.Sleep(30);
+                    }
+                }
+            }
+        }
+    }
+}
