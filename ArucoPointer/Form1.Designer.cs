5e654fe37222a840812423c68f6c772e33f03f79
watakooohhh3439 <bochuwatakoh3439@gmail.com>
2026-01-07 22:00:25 +0900

    22時現状

--- ArucoPointer/Form1.Designer.cs
+++ ArucoPointer/Form1.Designer.cs
@@ -59,13 +59,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(334, 3);
+            this.button1.Location = new System.Drawing.Point(441, 24);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(251, 44);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "再キャリブレーション\r\n";
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
